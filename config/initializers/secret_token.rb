48b63c15ab69e1ff3736a7c85655afc263fc5ca2
vkweberti <vkweberti@rezonans.(none)>
2013-12-10 08:11:00 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vogue::Application.config.secret_token = '3b3117641ff904a4ffb37d162ea122ebb9d08c9e49860dba7f5fad1ba9e0023e684fea3c280a974902dd62b7488f5ba4a18036c28e5a43169b7529d3142fd158'
